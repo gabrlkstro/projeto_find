94b890a82d5abea802f05d27f6ad4a352b470da1
Bruno Gomes <gabrielcastro3301@gmail.com>
2025-11-06 16:55:29 -0300

    login e registro

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('menu/', views.menu_view, name='menu'),
 ]
